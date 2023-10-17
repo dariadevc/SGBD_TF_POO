5254cd39d495281402aa1e34f6314ee51df8203f
Fiore <fiorellavald3z@gmail.com>
2023-10-17 16:17:15 -0300

    tabla modificada para mostrar datos de BD

--- Vista/VistaJefe/SeccionEmpleadoVista.py
+++ Vista/VistaJefe/SeccionEmpleadoVista.py
@@ -1,24 +1,32 @@
+from __future__ import annotations
 import sys
 from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QPushButton, QTableWidget, QTableWidgetItem, QLineEdit, QHeaderView
 from PyQt6.QtGui import QPixmap, QIcon
 from PyQt6.QtCore import Qt
 
-class SeccionEmpleadoVista (QMainWindow):
 
+class BotonNavegador (QPushButton):
+    def __init__(self, text: str, icon: str):
+        super().__init__()
+        self.setText(text)
+        self.setIcon(QIcon(icon))
+        self.setObjectName("boton-nav")
+
+class BotonAccionTabla (QPushButton):
+    def __init__(self, text: str, icon: str):
+        super().__init__()
+        self.setText(text)
+        self.setIcon(QIcon(icon))
+        self.setObjectName("boton-tabla")    
+
+class EncabezadoVistaJefe (QVBoxLayout): #encabezado común para todas las vistas de VistaJefe
+        
     def __init__(self):
         super().__init__()
 
-        widget_principal = QWidget()
-        self.setCentralWidget(widget_principal) #establece el widget principal de la ventana
-        self.setGeometry(100, 100, 800, 600) #establece dimensiones y posición de la ventana (x, y, ancho, alto)
-
-        #encabezado común para todas las vistas de VistaJefe --- ¿debería ir en una clase propia?
-        encabezado_layout = QVBoxLayout()
-
-        #elementos que contiene encabezado_layout
+        #elementos que contiene el encabezado
         banner_layout = QHBoxLayout()
         barra_nav_layout = QHBoxLayout()
-
 
         #banner | logo 
         logo = QPixmap("Vista/Media/logo-bsf.png")
@@ -30,7 +38,6 @@ class SeccionEmpleadoVista (QMainWindow):
         #banner | titulo
         nombre_refugio_label = QLabel("Best Friends")
         nombre_refugio_label.setObjectName("nombre_refugio_label")
-
 
         #banner | usuario_layout
         usuario_layout = QHBoxLayout()
@@ -53,34 +60,39 @@ class SeccionEmpleadoVista (QMainWindow):
         banner_layout.addStretch(1) #<-- añade un espacio elastico entre los elementos entre los que esté puesto, puede haber más de un addStretch para generar espacios de distintos tamaños
         banner_layout.addLayout(usuario_layout)
 
-
         #barra_nav | botones
-
-        boton_seccion_empleado = QPushButton("Empleados")
-        boton_seccion_animales = QPushButton("Animales")
-        boton_seccion_clientes = QPushButton("Clientes")
-        boton_seccion_citas = QPushButton("Citas")
-
-        boton_seccion_empleado.setIcon(QIcon("Vista/Media/icon-empleado.png"))
-        boton_seccion_animales.setIcon(QIcon("Vista/Media/icon-animales.png"))
-        boton_seccion_clientes.setIcon(QIcon("Vista/Media/icon-cliente.png"))
-        boton_seccion_citas.setIcon(QIcon("Vista/Media/icon-citas.png"))
-
-        boton_seccion_empleado.setObjectName("boton-nav")
-        boton_seccion_animales.setObjectName("boton-nav")
-        boton_seccion_clientes.setObjectName("boton-nav")
-        boton_seccion_citas.setObjectName("boton-nav")
+        boton_seccion_empleado = BotonNavegador("Empleados", "Vista/Media/icon-empleado.png")
+        boton_seccion_animales = BotonNavegador("Animales", "Vista/Media/icon-animales.png")
+        boton_seccion_clientes = BotonNavegador("Clientes", "Vista/Media/icon-cliente.png")
+        boton_seccion_citas = BotonNavegador("Citas", "Vista/Media/icon-citas.png")
 
         #barra_nav - composición
-        barra_nav_layout.addWidget(boton_seccion_empleado)
-        barra_nav_layout.addWidget(boton_seccion_animales)
-        barra_nav_layout.addWidget(boton_seccion_clientes)
-        barra_nav_layout.addWidget(boton_seccion_citas)
+        self.componer_layout(barra_nav_layout, [boton_seccion_empleado, boton_seccion_animales, boton_seccion_clientes, boton_seccion_citas])
 
-
+        #barra_nav - color fondo
         barra_nav_widget = QWidget()
         barra_nav_widget.setLayout(barra_nav_layout)
         barra_nav_widget.setObjectName("contenedor_botones_nav")
+
+        self.addLayout(banner_layout)
+        self.addWidget(barra_nav_widget)
+
+    def componer_layout (self, layout:QHBoxLayout|QVBoxLayout, widgets:list):
+        for w in widgets:
+            layout.addWidget(w) 
+
+
+class SeccionEmpleadoVista (QMainWindow):
+
+    info_tabla = []
+
+
+    def __init__(self):
+        super().__init__()
+
+        widget_principal = QWidget()
+        self.setCentralWidget(widget_principal) #establece el widget principal de la ventana
+        self.setGeometry(100, 100, 800, 600) #establece dimensiones y posición de la ventana (x, y, ancho, alto)
 
         #barra_busqueda | input de busqueda 
         barra_busqueda_layout = QHBoxLayout()
@@ -93,66 +105,72 @@ class SeccionEmpleadoVista (QMainWindow):
         barra_busqueda_layout.addWidget(input_busqueda)
         barra_busqueda_layout.setAlignment(input_busqueda, Qt.AlignmentFlag.AlignRight)
 
-
         #acciones_tabla_layout <-- acá estarán contenidos los botones y la tabla de datos
-        acciones_tabla_layout = QHBoxLayout()
+        acciones_usuario_layout = QHBoxLayout()
         acciones_botones_layout = QVBoxLayout()
 
             #acciones_botones_layout | botones 
-        agregar_e = QPushButton("   Agregar\n   empleado")
-        modificar_e = QPushButton(" Modificar\n empleado")
-        eliminar_e = QPushButton("  Eliminar\n  empleado")
+        agregar_e = BotonAccionTabla("   Agregar\n   empleado","Vista/Media/user-plus-svgrepo-com.png")
+        modificar_e = BotonAccionTabla(" Modificar\n empleado","Vista/Media/user-pen-svgrepo-com.png")
+        eliminar_e = BotonAccionTabla("  Eliminar\n  empleado","Vista/Media/user-xmark-svgrepo-com.png")
 
-        agregar_e.setIcon(QIcon("Vista/Media/user-plus-svgrepo-com.png"))
-        modificar_e.setIcon(QIcon("Vista/Media/user-pen-svgrepo-com.png"))
-        eliminar_e.setIcon(QIcon("Vista/Media/user-xmark-svgrepo-com.png"))
+        #acciones_tabla_layout - composición
+        self.componer_layout(acciones_botones_layout, [agregar_e, modificar_e, eliminar_e])
 
-        agregar_e.setObjectName("boton-tabla")
-        modificar_e.setObjectName("boton-tabla")
-        eliminar_e.setObjectName("boton-tabla")
-
-        acciones_botones_layout.addWidget(agregar_e)
-        acciones_botones_layout.addWidget(modificar_e)
-        acciones_botones_layout.addWidget(eliminar_e)
-        accion_bot = QWidget()
-        accion_bot.setLayout(acciones_botones_layout)
-        accion_bot.setObjectName("contenedor-botones-tabla")
-
+        acciones_botones_widget = QWidget()
+        acciones_botones_widget.setLayout(acciones_botones_layout)
+        acciones_botones_widget.setObjectName("contenedor-botones-tabla")
 
         #tabla
+        tabla_datos = QTableWidget(4,4) #4 filas y 4 columnas
+    
 
-        tabla_datos = QTableWidget()
-        tabla_datos.setRowCount(4)
-        tabla_datos.setColumnCount(4)
+        # info_tabla = [] <-- atributo de clase que estoy usando
 
-        encabezados_tabla = ["DNI", "APELLIDO", "NOMBRE", "FECHA DE INGRESO"]
-        tabla_datos.setHorizontalHeaderLabels(encabezados_tabla)
+        tabla_datos.setRowCount(len(SeccionEmpleadoVista.info_tabla))
+        tabla_datos.setColumnCount(len(SeccionEmpleadoVista.info_tabla[0]))
+        tabla_datos.setHorizontalHeaderLabels(["DNI", "APELLIDO", "NOMBRE", "FECHA DE INGRESO"])
+        tabla_datos.verticalHeader().setVisible(False)
 
-        for col in range(3):
+        for row_idx, row in enumerate(SeccionEmpleadoVista.info_tabla):
+            for col_idx, col_data in enumerate(row):
+                item = QTableWidgetItem(str(col_data))
+                item.setFlags(item.flags() ^ Qt.ItemFlag.ItemIsEditable)
+                tabla_datos.setItem(row_idx, col_idx, item)
+
+
+        # tabla_datos.setHorizontalHeaderLabels(["DNI", "APELLIDO", "NOMBRE", "FECHA DE INGRESO"])
+
+        for col in range(4):
             tabla_datos.horizontalHeader().setSectionResizeMode(col, QHeaderView.ResizeMode.Stretch)
 
         tabla_datos.setColumnWidth(3, 200) #para que el ancho no corte el titulo de la columna en esa columna
 
-        for i in range(4):
-            for j in range(4):
-                tabla_datos.setItem(i, j, QTableWidgetItem(f"Celda {i}, {j}"))
-
+        # for i in range(tabla_datos.rowCount()):
+        #     for j in range(tabla_datos.columnCount()):
+        #         tabla_datos.setItem(i, j, QTableWidgetItem(f"Celda {i}, {j}"))
 
         #acciones_tabla_layout - composición
-        acciones_tabla_layout.addWidget(accion_bot)
-        acciones_tabla_layout.addWidget(tabla_datos)
+        acciones_usuario_layout.addWidget(acciones_botones_widget)
+        acciones_usuario_layout.addWidget(tabla_datos)
 
 
-        #encabezado_layout - composición
-        encabezado_layout.addLayout(banner_layout)
-        encabezado_layout.addWidget(barra_nav_widget)
-        
+        encabezado_layout = EncabezadoVistaJefe() #construye el encabezado
 
         #layout principal - del widget principal
         layout_principal = QVBoxLayout()
         layout_principal.addLayout(encabezado_layout)
         layout_principal.addLayout(barra_busqueda_layout)
-        layout_principal.addLayout(acciones_tabla_layout)
+        layout_principal.addLayout(acciones_usuario_layout)
 
         widget_principal.setLayout(layout_principal)
+
+    def componer_layout (self, layout, widgets):
+        for w in widgets:
+            layout.addWidget(w)  
+
+    @classmethod
+    def set_tabla_datos (cls, datos):
+        cls.info_tabla.extend(datos)
+
         
