383ad4c13550cd0245cacb9c0320c902578e68d4
Fiore <fiorellavald3z@gmail.com>
2023-10-17 16:14:45 -0300

    Añadido metodo de consulta BD

--- Controlador/ControladorJefe/ControladorSeccionEmpleado.py
+++ Controlador/ControladorJefe/ControladorSeccionEmpleado.py
@@ -1,11 +1,14 @@
 import sys
 from PyQt6.QtWidgets import QApplication
 from Vista.VistaJefe.SeccionEmpleadoVista import SeccionEmpleadoVista
+from Modelo.DataBase import DataBase
 
 class ControladorSeccionEmpleado:
 
     def __init__(self, estilo):
         app = QApplication(sys.argv)
+        self.__base = DataBase()
+        self.obtener_empleados()
         self.__window = SeccionEmpleadoVista()
         self.__window.setWindowTitle("Best Friends")
         self.__window.show()
@@ -13,3 +16,6 @@ class ControladorSeccionEmpleado:
         with open(estilo) as f:
             app.setStyleSheet(f.read())
         app.exec()
+
+    def obtener_empleados(self):
+        SeccionEmpleadoVista.set_tabla_datos(self.__base.getAll("SELECT * FROM empleado"))
