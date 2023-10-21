9d7c0f046d9b38fdf095dd48470e1d49b520a8e7
Daria <dsauco36@gmail.com>
2023-10-20 23:53:26 -0300

    usuarios

---
+++ Controlador/Personas/Persona.py
@@ -0,0 +1,9 @@
+
+class Persona:
+    
+    def __init__ (self, dni, nombre, apellido):
+        # self.id_persona = 
+        self.dni = dni
+        self.nombre = nombre
+        self.apellido = apellido
+        
