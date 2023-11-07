3dae51c6478e3a1cca8c965256a004d9d413c3e8
Daria Sauco <dsauco36@gmail.com>
2023-11-07 17:46:12 -0300

    ABM: base de datos actualizada

--- Controlador/ControladorJefe/ControladorSeccionEmpleado1.py
+++ Controlador/ControladorJefe/ControladorSeccionEmpleado1.py
@@ -1,11 +1,10 @@
 import sys
 from PyQt6.QtWidgets import QApplication
-from Vista.VistaJefe.SeccionEmpleadoVista1 import SeccionEmpleadoVista
+from Vista.vista_secciones.SeccionEmpleadoVista1 import SeccionEmpleadoVista
 from Modelo.DataBase import DataBase
 
 
 class ControladorSeccionEmpleado:
-
     def __init__(self, estilo):
         # app = QApplication(sys.argv)
         self.__base = DataBase()
@@ -17,19 +16,28 @@ class ControladorSeccionEmpleado:
         self.__window.get_input_busqueda().editingFinished.connect(self.buscar_empleado)
         self.__window.get_input_busqueda().textChanged.connect(self.buscar_empleado)
 
-    
         with open(estilo) as f:
             self.__window.setStyleSheet(f.read())
         # app.exec()
 
     def obtener_empleados(self):
-        SeccionEmpleadoVista.set_tabla_datos(self.__base.getAll("SELECT dni, nombre_usuario, nombre, apellido FROM public.usuarios"))
-        
+        SeccionEmpleadoVista.set_tabla_datos(
+            self.__base.getAll(
+                "SELECT dni, nombre_usuario, nombre, apellido FROM public.usuarios"
+            )
+        )
+
     # def get_ventana (self):
     #     return self.__window
-    
+
     # def mostrar_ventana (self):
     #     return self.__window.show()
 
     def buscar_empleado(self):
-        SeccionEmpleadoVista.set_tabla_datos(self.__base.getAll("SELECT dni, nombre_usuario, nombre, apellido FROM public.usuarios WHERE nombre_usuario = '{}'".format(self.__window.obtener_usuario_buscado())))
+        SeccionEmpleadoVista.set_tabla_datos(
+            self.__base.getAll(
+                "SELECT dni, nombre_usuario, nombre, apellido FROM public.usuarios WHERE nombre_usuario = '{}'".format(
+                    self.__window.obtener_usuario_buscado()
+                )
+            )
+        )
