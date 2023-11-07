c42a91f96d2b92e1f6f8854351223cf0cb08ad7a
Daria Sauco <dsauco36@gmail.com>
2023-11-07 18:23:07 -0300

    builder: cambios

--- Controlador/ControladorJefe/ControladorSeccionEmpleado.py
+++ Controlador/ControladorJefe/ControladorSeccionEmpleado.py
@@ -1,24 +1,43 @@
 import sys
 from PyQt6.QtWidgets import QApplication
-from Vista.VistaJefe.SeccionEmpleadoVista import SeccionEmpleadoVista
+from Vista.vista_secciones.SeccionEmpleadoVista1 import SeccionEmpleadoVista
 from Modelo.DataBase import DataBase
 
-class ControladorSeccionEmpleado:
 
+class ControladorSeccionEmpleado:
     def __init__(self, estilo):
-        app = QApplication(sys.argv)
+        # app = QApplication(sys.argv)
         self.__base = DataBase()
         self.obtener_empleados()
         self.__window = SeccionEmpleadoVista()
         self.__window.setWindowTitle("Best Friends")
         self.__window.show()
+        # empleado = self.__window.obtener_usuario_buscado()
+        self.__window.get_input_busqueda().editingFinished.connect(self.buscar_empleado)
+        self.__window.get_input_busqueda().textChanged.connect(self.buscar_empleado)
 
         with open(estilo) as f:
-            app.setStyleSheet(f.read())
-        app.exec()
+            self.__window.setStyleSheet(f.read())
+        # app.exec()
 
     def obtener_empleados(self):
-        SeccionEmpleadoVista.set_tabla_datos(self.__base.getAll("SELECT * FROM empleado"))
-        
-    
+        SeccionEmpleadoVista.set_tabla_datos(
+            self.__base.getAll(
+                "SELECT dni_usuario, nombre_usuario, apellido_usuario, alias_usuario FROM public.usuario"
+            )
+        )
 
+    # def get_ventana (self):
+    #     return self.__window
+
+    # def mostrar_ventana (self):
+    #     return self.__window.show()
+
+    def buscar_empleado(self):
+        SeccionEmpleadoVista.set_tabla_datos(
+            self.__base.getAll(
+                "SELECT dni, nombre_usuario, nombre, apellido FROM public.usuarios WHERE nombre_usuario = '{}'".format(
+                    self.__window.obtener_usuario_buscado()
+                )
+            )
+        )
--- Modelo/UsuarioDao.py
+++ Modelo/UsuarioDao.py
@@ -1,19 +1,23 @@
 from Modelo.DataBase import DataBase
 
-class UsuarioDao:
 
+class UsuarioDao:
     def __init__(self):
         self.base = DataBase()
-    
+
     # def get_all(self):
     #     return self.base.getAll("SELECT * FROM Usuarios")
 
     # def get(self, id):
     #     return self.base.get("SELECT * FROM Usuarios WHERE id = {}".format(id))
-    
+
     def login(self, usuario, contrasenia):
-        return self.base.get("SELECT * FROM public.usuarios as d WHERE nombre_usuario = '{}' and contrasenia  = '{}'".format(usuario, contrasenia))
-    
+        return self.base.get(
+            "SELECT * FROM public.usuario as d WHERE alias_usuario = '{}' and contrasenia_usuario  = '{}'".format(
+                usuario, contrasenia
+            )
+        )
+
     # def insert(self):
     #     pass
 
