5960e667271350d6ca63ae7f17c78ffb22321085
Fiore <fiorellavald3z@gmail.com>
2023-11-14 20:54:52 -0300

    borrado

--- main.py
+++
@@ -1,8 +0,0 @@
-from PyQt6.QtWidgets import QApplication
-import sys
-from Controlador.controlador_login.controlador_login import ControladorLogin
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    controlador = ControladorLogin("Vista/VistaLogin/estilo_login.qss")
-    app.exec()
